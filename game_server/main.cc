46a029874dc36cb47c62d80f9a353a43f87d36f4
lvbin <binlv@123u.com>
2019-08-02 10:38:04 +0800

    网络部分

---
+++ game_server/main.cc
@@ -0,0 +1,21 @@
+#include "evpp/logging.h"
+
+void Init()
+{
+	/*加载全局配置文件*/
+
+	/*初始化日志*/
+	//google::InitGoogleLogging("aa");
+	
+}
+
+int main(int argc, char* argv[]) {
+
+	
+
+	Init();
+
+	/*启动选中的服务*/
+
+    return 0;
+}
